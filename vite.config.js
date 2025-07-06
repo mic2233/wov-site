2784a7609ab60c79da535f12eaf8fce5ac794234
mic2233 <m2211665@gmail.com>
2025-07-06 11:08:41 +0200

    installed tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';   // ← make sure this line is here
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],                       // ← …and this one
+
+});
