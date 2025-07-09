a82e5cd77cb0fea10a82795deed6b2289ac152b4
mic2233 <m2211665@gmail.com>
2025-07-09 12:33:28 +0200

    fixed the navbar look

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,16 +9,15 @@ function NavBar() {
       <div className="navbar ">
         <img className="voxlogo" src={logo} alt="Wolfbard logo" />
         <ul className="menu menu-horizontal ">
-          <li><a href="#bio ">Bio</a></li>
-          <li><a href="#music">Music</a></li>
-          <li><a href="#contact">Contact</a></li>
-          <li>  <a
+          <li> <a tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1"  > Bio</a></li>
+          <li> <a tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1"  > Music</a></li>
+          <li> <a tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1"  > Contact</a></li>
+          <li> <a tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1"  > Shop</a></li>
+          <li> <a  tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1 tooltip tooltip-bottom "
                 target="_blank"
                 rel="noopener noreferrer"
-                className="tooltip tooltip-bottom " 
                 data-tip="Coming Soon !"
-                
-              >Shop</a></li>
+              > Shop</a></li>
           <li><a href="https://www.youtube.com/@Wolfbard_Of_Veles/videos">Music Videos</a></li>
         </ul>
       </div>
