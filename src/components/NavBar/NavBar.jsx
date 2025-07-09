c9e66a9e8203f7aa1f7d84e51723fe1221e342df
mic2233 <m2211665@gmail.com>
2025-07-09 15:44:09 +0200

    updated bio section on the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './NavBar.css'
-import  logo from '../../assets/WOV_logo.PNG' 
+import logo from '../../assets/WOV_logo.PNG'
 function NavBar() {
   const [count, setCount] = useState(0)
 
@@ -9,16 +9,43 @@ function NavBar() {
       <div className="navbar ">
         <img className="voxlogo" src={logo} alt="Wolfbard logo" />
         <ul className="menu menu-horizontal ">
-          <li> <a tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1"  > Bio</a></li>
+          <li className="dropdown dropdown-center dropdown-hover">
+            <a
+              tabIndex={0} role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1">
+                Bio
+            </a>
+            <ul className="dropdown-content p-4 shadow glass rounded-box w-5xl">
+              <div className="card card-side  ">
+                <figure className="pl-2 pt-2 ml-5 mt-3 mb-5">
+                  <img
+                    src="wov-site\src\assets\Wolfbard_of_veles_main_pic.png"
+                    alt="Album cover"
+                    className=" bio-picture opacity-90 w-110 h-110 object-cover rounded-lg mask-16"
+                  />
+                </figure>
+                <div className="card-body ml-5 mt-1 mb-5">
+                  <h4 className="card-title text-2xl">     
+                    Folk <br />
+                    DOOM <br />
+                    PROG METAL<br /></h4>
+   
+                  <p className="bio-text text-[18px] absolute right-18 bottom-25 h-16">
+                    I ‘m a passionate music producer and guitarist, <br />
+                    with a deep-rooted love for crafting soundscapes in <br />
+                    the realms of folk, progressive rock, metal and doom<br />
+                  </p>
+                </div>
+              </div>
+            </ul>
+          </li>
           <li> <a tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1"  > Music</a></li>
           <li> <a tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1"  > Contact</a></li>
           <li> <a tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1"  > Shop</a></li>
-          <li> <a  tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1 tooltip tooltip-bottom "
-                target="_blank"
-                rel="noopener noreferrer"
-                data-tip="Coming Soon !"
-              > Shop</a></li>
-          <li><a href="https://www.youtube.com/@Wolfbard_Of_Veles/videos">Music Videos</a></li>
+          <li> <a tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1 tooltip tooltip-bottom"
+            target="_blank"
+            rel="noopener noreferrer"
+            data-tip="Coming Soon !"> Shop</a></li>
+          <li> <a href="https://www.youtube.com/@Wolfbard_Of_Veles/videos" tabindex="0" role="button" className="btn btn-xs btn-ghost hover:bg-transparent m-1"> Music Videos</a></li>
         </ul>
       </div>
     </>
@@ -26,3 +53,4 @@ function NavBar() {
 }
 
 export default NavBar
+
